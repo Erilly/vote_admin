b0bb500032edb78a6c7fead55c9d5010fae03489
yangjie-bt <young0303@yeah.net>
2019-03-07 19:20:43 +0800

    login

--- controllers/login.go
+++ controllers/login.go
@@ -11,3 +11,17 @@ type LoginController struct {
 func (this *LoginController) Get() {
 	this.TplName = "admin/vote/login.html"
 }
+
+func (this *LoginController) Post() {
+
+	username:=this.Input().Get("username")
+	password:=this.Input().Get("password")
+
+	if beego.AppConfig.String("adminuser") == username &&
+		beego.AppConfig.String("adminpass") == password{
+		this.Ctx.SetCookie("username",username,0)
+		this.Ctx.SetCookie("password",password,0)
+	}
+	this.Redirect("/",301)
+	return
+}
